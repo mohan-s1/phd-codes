ec318e11a5e8c461152b30abdd721389dc62c984
Mohan <118559061+mohan-s1@users.noreply.github.com>
2026-09-17 22:33:33 -0400

    Clean up restart_metadynamics.py

--- MD/metadynamics/restart_metadynamics.py
+++ MD/metadynamics/restart_metadynamics.py
@@ -23,12 +23,12 @@ from mace.calculators import MACECalculator
 from ase.io.trajectory import Trajectory
 
 import torch
+import numpy as np
 # ------------------------------------------------------------
 # Settings
 # ------------------------------------------------------------
 
 timestep = 0.5 * units.fs
-temperature = 648
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
@@ -45,6 +45,24 @@ previous_steps = n_frames * 100   # last fully-checkpointed step
 
 atoms = read("md.traj", index=-1)
 
+Al_indices = np.where(np.array(atoms.get_chemical_symbols()) == 'Al')[0]
+Co_indices = np.where(np.array(atoms.get_chemical_symbols()) == 'Co')[0]
+
+# <Al_ASE_idx, Co_ASE_idx> + 1
+plumed_Al_index = Al_indices[0] + 1
+plumed_Co_index = Co_indices[0] + 1
+
+# Enable periodic boundary conditions in all three directions
+atoms.set_pbc([True, True, True])
+
+# Make sure the unit cell is present
+print("Cell:", atoms.cell)
+print("PBC:", atoms.pbc)
+
+temperature_kelvin = 600
+num_steps = 1_000_000
+dump_frequency = 100
+
 # ------------------------------------------------------------
 # Base calculator
 # ------------------------------------------------------------
@@ -56,23 +74,30 @@ base_calc = MACECalculator(model_paths=["Co_NH3_stagetwo_compiled.model"], devic
 # PLUMED setup
 # ------------------------------------------------------------
 
+# setup = [
+#     f"RESTART\n",
+
+#     "d: DISTANCE ATOMS=289,583\n",
+
+#     "metad: METAD "
+#     "ARG=d "
+#     "SIGMA=0.03 "
+#     "HEIGHT=1.5 "
+#     "PACE=500 "
+#     "BIASFACTOR=10 "
+#     "TEMP={} "
+#     "GRID_MIN=0.2 "
+#     "GRID_MAX=1.5 "
+#     "GRID_BIN=500 "
+#     "FILE=HILLS\n",
+
+#     "PRINT ARG=d,metad.bias STRIDE=100 FILE=COLVAR\n",
+# ]
+
 setup = [
-    "RESTART\n",
-
-    "d: DISTANCE ATOMS=289,583\n",
-
-    "metad: METAD "
-    "ARG=d "
-    "SIGMA=0.03 "
-    "HEIGHT=1.5 "
-    "PACE=500 "
-    "BIASFACTOR=10 "
-    "TEMP=648 "
-    "GRID_MIN=0.2 "
-    "GRID_MAX=1.5 "
-    "GRID_BIN=500 "
-    "FILE=HILLS\n",
-
+    "RESTART\n"
+    f"d: DISTANCE ATOMS={plumed_Co_index},{plumed_Al_index}\n", 
+    f"metad: METAD ARG=d SIGMA=0.03 HEIGHT=1.5 PACE=500 BIASFACTOR=10 TEMP={temperature_kelvin} GRID_MIN=0.1 GRID_MAX=2.0 GRID_BIN=400 FILE=HILLS\n",
     "PRINT ARG=d,metad.bias STRIDE=100 FILE=COLVAR\n",
 ]
 
@@ -86,7 +111,7 @@ calc = Plumed(
     input=setup,
     timestep=timestep,
     atoms=atoms,
-    kT=temperature * units.kB,
+    kT=temperature_kelvin * units.kB,
     log="plumed.log",
     restart=True,
 )
@@ -104,7 +129,7 @@ atoms.calc = calc
 dyn = NoseHooverChainNVT(
     atoms,
     timestep=timestep,
-    temperature_K=temperature,
+    temperature_K=temperature_kelvin,
     tdamp=100 * units.fs,
 )
 
@@ -113,8 +138,8 @@ dyn = NoseHooverChainNVT(
 from ase.io.trajectory import Trajectory
 
 traj = Trajectory("md.traj", "a", atoms)
-dyn.attach(traj.write, interval=100)
+dyn.attach(traj.write, interval=dump_frequency)
 
 
 # Run another 1,000,000 steps
-dyn.run(1_000_000)
+dyn.run(num_steps)
